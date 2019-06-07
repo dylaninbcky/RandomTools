dd79f9aed99d2154f6ecd001465e53e35833d8a3
dylaninbcky <dylanberghuis1@gmail.com>
2019-06-07 09:11:05 +0200

    Wrapper voor Nmap scans, custom objects voor html reports

---
+++ Format-NmapScan.ps1
@@ -0,0 +1,24 @@
+Function Format-NmapScan {
+    param (
+    [parameter(Mandatory = $true, Position = 0,
+        Helpmessage = "Volledige command voor port outputting",
+        ValueFromPipeline)][scriptblock]$Command
+    )
+    PROCESS {
+        $stringcommand  = $Command.ToString().Trim() + " -OutFormat HashTable"
+        $results = Invoke-Expression -Command $stringcommand
+        $output = @()
+        for ($i = 0; $i -lt $results.host.ports.port.portid.Length; $i++) {
+            $output += [PSCustomObject]@{
+                Poorten  = $results.host.ports.port.portid[$i]
+                Protocol = $results.host.ports.port.protocol[$i]
+                Target   = $results.host.address.addr  
+            }
+        }
+      return $output
+    }
+}
+
+Format-NmapScan -Command {
+    Invoke-Nmap -computerName www.google.com
+}
