e7d240b18cc2776d511aa7cc7ffd9b62651dd43b
Dylan Berghuis <47518792+dylaninbcky@users.noreply.github.com>
2022-06-15 14:21:55 +0200

    Create Get-Customteamviewer.ps1

---
+++ Get-Customteamviewer.ps1
@@ -0,0 +1,31 @@
+$downloadpath = "$ENV:HOMEPATH\Desktop"
+$URL = "https://get.teamviewer.com/u6pkngy"
+Function Start-TeamViewerQSDownload () {
+    $iebrowser = New-Object -ComObject InternetExplorer.Application
+    $iebrowser.Navigate($URL)
+    Start-Sleep -Seconds 3
+    $CustomTeamviewer = $iebrowser.Document.getElementById('MasterBodyContent_btnRetry').href
+    Start-BitsTransfer -source $CustomTeamviewer -Destination $downloadpath\TeamViewerQS.exe       
+}
+
+Function Set-TeamviewerQS{
+    param(
+        $downloadpath,
+        $customURL
+    )
+    try {
+        if (Test-Path $downloadpath){
+            Start-TeamViewerQSDownload
+        }
+    }
+    catch{
+        Write-Output 'cannot download teamviewer'
+    }
+}
+
+if  (!(Test-path "$downloadpath\TeamviewerQS.exe")){
+    Set-TeamviewerQS -downloadpath $downloadpath -customURL $URL
+}
+else{
+    Write-host 'Teamviewer already installed' -ForegroundColor Green
+}
